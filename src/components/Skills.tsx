edee5cc899ea4972ac8d63a1af518603a025bb5c
nerwinalamas <nerwinalamas@gmail.com>
2024-11-03 19:52:24 +0800

    refactor: filter by category and add animation

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,74 @@
+import { useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { motion } from "framer-motion";
 import { skillsData } from "../data";
+import { fadeIn, fadeInUp, fadeInWithExit } from "../lib/animation";
 
 const Skills = () => {
+    const [filter, setFilter] = useState("All");
+
+    const filteredProjects =
+        filter === "All"
+            ? skillsData
+            : skillsData.filter((skill) => skill.category === filter);
+
     return (
-        <div id="skills" className="p-4 my-40 flex flex-col gap-10">
-            <h2 className="text-5xl font-bold">Skills</h2>
+        <motion.div
+            id="skills"
+            {...fadeInUp}
+            className="p-4 my-40 flex flex-col gap-10"
+        >
+            <div className="flex flex-col gap-5">
+                <motion.h1
+                    {...fadeIn}
+                    transition={{ ...fadeIn.transition, delay: 0.2 }}
+                    className="text-5xl font-bold"
+                >
+                    Skills
+                </motion.h1>
+                <Tabs defaultValue="All" className="w-full">
+                    <TabsList className="h-max flex-wrap bg-slate-900">
+                        <TabsTrigger
+                            value="All"
+                            onClick={() => setFilter("All")}
+                        >
+                            All
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="frontend"
+                            onClick={() => setFilter("frontend")}
+                        >
+                            Frontend
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="backend"
+                            onClick={() => setFilter("backend")}
+                        >
+                            Backend
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="state management & data handling"
+                            onClick={() =>
+                                setFilter("state management & data handling")
+                            }
+                        >
+                            State Management & Data Handling
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="development tools"
+                            onClick={() => setFilter("development tools")}
+                        >
+                            Development Tools
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </div>
             <div className="flex flex-wrap gap-5 justify-center">
-                {skillsData.map((data) => (
-                    <div
+                {filteredProjects.map((data, index) => (
+                    <motion.div
                         key={data.id}
+                        {...fadeInWithExit}
+                        transition={fadeInWithExit.transition(index)}
                         className="w-32 h-32 p-4 rounded-md flex flex-col items-center justify-center gap-5 bg-slate-800"
                     >
                         <img
@@ -16,10 +77,10 @@ const Skills = () => {
                             className="w-10 h-10"
                         />
                         <p className={`text-center text-xs`}>{data.title}</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
-        </div>
+        </motion.div>
     );
 };
 

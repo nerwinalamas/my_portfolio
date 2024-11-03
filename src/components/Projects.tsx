be5dd105b1d3225feb2d2fe41870be9d2368f401
nerwinalamas <nerwinalamas@gmail.com>
2024-11-03 19:52:42 +0800

    refactor: project card and add animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { projectsData } from "../data";
 import { Badge } from "./ui/badge";
@@ -8,13 +9,34 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "./ui/carousel";
+import { Button } from "./ui/button";
+import { ChevronDown, ChevronUp, ExternalLink, Github } from "lucide-react";
+import { motion } from "framer-motion";
+import { fadeIn, fadeInUp } from "../lib/animation";
 
 const Projects = () => {
+    const [expandedProjects, setExpandedProjects] = useState<
+        Record<string, boolean>
+    >({});
+
+    const toggleProjectDetails = (projectId: string) => {
+        setExpandedProjects((prev) => ({
+            ...prev,
+            [projectId]: !prev[projectId],
+        }));
+    };
+
     return (
-        <div id="projects" className="p-4 my-40">
+        <motion.div id="projects" {...fadeInUp} className="p-4 my-40">
             <Carousel className="w-full flex flex-col gap-10">
                 <div className="flex justify-between">
-                    <h2 className="text-3xl lg:text-5xl font-bold">Projects</h2>
+                    <motion.h1
+                        {...fadeIn}
+                        transition={{ ...fadeIn.transition, delay: 0.2 }}
+                        className="text-3xl lg:text-5xl font-bold"
+                    >
+                        Projects
+                    </motion.h1>
                     <div className="flex gap-5">
                         <CarouselPrevious className="w-10 h-10 rounded-full" />
                         <CarouselNext className="w-10 h-10 rounded-full" />
@@ -26,63 +48,106 @@ const Projects = () => {
                             key={project.id}
                             className="md:basis-1/2 lg:basis-1/3"
                         >
-                            <div className="p-2 flex flex-col rounded-lg overflow-hidden hover:bg-slate-800/50">
+                            <div className="p-2 flex flex-col rounded-lg border border-slate-800 hover:bg-slate-800/50">
                                 <img
                                     src={project.image}
                                     alt={project.name}
                                     className="object-cover border rounded-lg border-slate-800/90"
                                 />
-                                <div className="flex flex-col gap-2 p-4">
+                                <div className="h-full flex flex-col gap-2 p-4">
                                     <h3 className="text-xl font-bold">
                                         {project.name}
                                     </h3>
-                                    <p>{project.description}</p>
-                                    {project.email && project.password && (
-                                        <h4 className="font-semibold text-teal-500">
-                                            Login Credentials:
-                                        </h4>
+                                    <p className="min-h-24">
+                                        {project.description}
+                                    </p>
+                                    <button
+                                        onClick={() =>
+                                            toggleProjectDetails(project.id)
+                                        }
+                                        className="w-max mb-2 flex items-center gap-2 hover:text-slate-400 transition-colors"
+                                    >
+                                        {expandedProjects[project.id]
+                                            ? "see less"
+                                            : "see more"}{" "}
+                                        {expandedProjects[project.id] ? (
+                                            <ChevronUp className="size-4" />
+                                        ) : (
+                                            <ChevronDown className="size-4" />
+                                        )}
+                                    </button>
+
+                                    {expandedProjects[project.id] && (
+                                        <div className="flex flex-col gap-4 animate-in fade-in slide-in-from-top-1 duration-200 mb-2">
+                                            {(project.email ||
+                                                project.password) && (
+                                                <div className="space-y-1">
+                                                    <h4 className="font-semibold text-teal-500">
+                                                        Login Credentials:
+                                                    </h4>
+                                                    {project.email && (
+                                                        <p>
+                                                            Email:{" "}
+                                                            <span className="font-bold">
+                                                                {project.email}
+                                                            </span>
+                                                        </p>
+                                                    )}
+                                                    {project.password && (
+                                                        <p>
+                                                            Password:{" "}
+                                                            <span className="font-bold">
+                                                                {
+                                                                    project.password
+                                                                }
+                                                            </span>
+                                                        </p>
+                                                    )}
+                                                </div>
+                                            )}
+
+                                            <div className="flex flex-wrap gap-3">
+                                                {project.techStack?.map(
+                                                    (tech) => (
+                                                        <Badge
+                                                            variant="secondary"
+                                                            key={tech}
+                                                        >
+                                                            {tech}
+                                                        </Badge>
+                                                    )
+                                                )}
+                                            </div>
+                                        </div>
                                     )}
-                                    {project.email && (
-                                        <p>
-                                            Email:{" "}
-                                            <span className="font-bold">
-                                                {project.email}
-                                            </span>
-                                        </p>
-                                    )}
-                                    {project.password && (
-                                        <p>
-                                            Password:{" "}
-                                            <span className="font-bold">
-                                                {project.password}
-                                            </span>
-                                        </p>
-                                    )}
-                                    <div className="flex flex-wrap gap-3">
-                                        {project.techStack.map((tech) => (
-                                            <Badge
-                                                variant="secondary"
-                                                key={tech}
+
+                                    <div className="flex flex-col gap-5 md:flex-row mt-auto">
+                                        <Button
+                                            variant="secondary"
+                                            className="w-full flex items-center justify-center gap-2"
+                                            asChild
+                                        >
+                                            <Link
+                                                to={project.demo}
+                                                target="_blank"
                                             >
-                                                {tech}
-                                            </Badge>
-                                        ))}
-                                    </div>
-                                    <div className="mt-5 flex flex-col gap-5 md:flex-row">
-                                        <Link
-                                            to={project.demo}
-                                            target="_blank"
-                                            className="px-7 py-2 text-center rounded-md hover:underline text-slate-800 bg-slate-100"
+                                                <ExternalLink className="size-4" />
+                                                Demo
+                                            </Link>
+                                        </Button>
+                                        <Button
+                                            variant="ghost"
+                                            className="w-full flex items-center justify-center gap-2"
+                                            asChild
                                         >
-                                            Demo
-                                        </Link>
-                                        <Link
-                                            to={project.repo}
-                                            target="_blank"
-                                            className="px-7 py-2 text-center rounded-md hover:underline text-slate-100 bg-slate-900 border"
-                                        >
-                                            Repo
-                                        </Link>
+                                            <Link
+                                                to={project.repo}
+                                                target="_blank"
+                                            >
+                                                <Github className="size-4" />
+                                                Repo
+                                            </Link>
+                                        </Button>
                                     </div>
                                 </div>
                             </div>
@@ -90,7 +155,7 @@ const Projects = () => {
                     ))}
                 </CarouselContent>
             </Carousel>
-        </div>
+        </motion.div>
     );
 };
 

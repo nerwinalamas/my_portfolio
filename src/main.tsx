3557f2a5917c2fe3128884afa6ae8f57db484798
nerwinalamas <nerwinalamas@gmail.com>
2024-11-04 10:03:14 +0800

    refactor: add react lazy load image component css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

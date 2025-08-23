2341dc297aa8be5195f015285f457c9e2c1baa98
Nerwin Alamas <nerwinalamas@gmail.com>
2025-08-23 09:16:07 +0800

    refactor: export the nearest throne image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -7,6 +7,7 @@ import threeS from "./3s.png"
 import netflixClone from "./netflix_clone.png"
 import id from "./id.png"
 import flowboard from "./flowboard.png"
+import theNearestThrone from "./the_nearest_throne.png"
 
 export {
     paAppointment,
@@ -18,4 +19,5 @@ export {
     netflixClone,
     id,
     flowboard,
+    theNearestThrone,
 }
